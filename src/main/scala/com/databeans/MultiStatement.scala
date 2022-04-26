e1405f2efcbab78674b040177e46b3fe5db01a86
gargouriahmed <ahmed.gargouri@dsoservices.com>
2022-04-26 03:15:26 +0100

    multi-statement running without issues when there are no conflicts

--- src/main/scala/com/databeans/MultiStatement.scala
+++ src/main/scala/com/databeans/MultiStatement.scala
@@ -3,8 +3,25 @@ package com.databeans
 import org.apache.spark.sql.SparkSession
 
 object MultiStatement {
+
+  /*def revert(spark: SparkSession, tableNames: Array[String]): Unit = {
+    import spark.implicits._
+    for (i <- tableNames.indices)
+    {
+      val deltaTable = DeltaTable.forName(spark, tableNames(i))
+      deltaTable.restoreToversion(123)
+    }
+  }*/
+
  def beginTransaction(spark : SparkSession, transactions: Array[String]): Unit = {
-   for (i <- transactions.indices)
-   {spark.sql(transactions(i))}
+   try
+     {
+       for (i <- transactions.indices)
+       {spark.sql(transactions(i))}
+     }
+   catch {
+     case error: UnsupportedOperationException =>
+       {print("UnsupportedOperationException")}   //revert changes done and give up doing the rest
+   }
  }
 }
--- src/test/scala/com/databeans/MultiStatementSpec.scala
+++ src/test/scala/com/databeans/MultiStatementSpec.scala
@@ -11,10 +11,10 @@ class MultiStatementSpec extends QueryTest
  test("beginTransaction should run multiple SQL queries") {
    val s = spark
    import s.implicits._
-   val data = Seq(1 , 100).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
+   val data = Seq(1, 5, 100).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
    data.write.format("delta").saveAsTable("my_fake_tab")
 
-   val updatesData = Seq(98 , 102).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
+   val updatesData = Seq(98, 5, 102).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
    updatesData.write.mode("append").partitionBy("option").format("delta").saveAsTable("updates")
 
    val mergeQuery = """
@@ -27,10 +27,37 @@ class MultiStatementSpec extends QueryTest
                   |  THEN INSERT (value, keys, option) VALUES (value, keys, option)
                   |""".stripMargin
 
-   val scanQuery = "select * from my_fake_tab"
+   val deleteQuery = "DELETE FROM my_fake_tab WHERE value = 5"
 
-   beginTransaction(spark, Array(mergeQuery,  scanQuery))
+   beginTransaction(spark, Array(deleteQuery, mergeQuery))
    Thread.sleep(5000)
    spark.read.format("delta").table("my_fake_tab").show()
  }
+
+  test("beginTransaction should run multiple SQL queries when there are conflicts") {
+    val s = spark
+    import s.implicits._
+    val data = Seq(1, 5, 100).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
+    data.write.format("delta").saveAsTable("my_fake_tab")
+
+    val updatesData = Seq(98, 5, 102).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
+    updatesData.write.mode("append").partitionBy("option").format("delta").saveAsTable("updates")
+
+    val mergeQuery = """
+                       |MERGE INTO my_fake_tab
+                       |USING updates
+                       |ON my_fake_tab.value = updates.value
+                       |WHEN MATCHED THEN
+                       |  UPDATE SET my_fake_tab.value = updates.value
+                       |WHEN NOT MATCHED
+                       |  THEN INSERT (value, keys, option) VALUES (value, keys, option)
+                       |""".stripMargin
+
+    val updateQuery = "UPDATE updates SET value = 5 WHERE value = 98"
+
+    beginTransaction(spark, Array(updateQuery, mergeQuery))
+    Thread.sleep(5000)
+    spark.read.format("delta").table("my_fake_tab").show()
+    spark.read.format("delta").table("updates").show()
+  }
 }
