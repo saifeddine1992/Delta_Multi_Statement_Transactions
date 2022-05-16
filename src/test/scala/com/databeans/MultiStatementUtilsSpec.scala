f5f42a4d7c4587a8315a57aa30f5d3ad5f931404
gargouriahmed <ahmed.gargouri@dsoservices.com>
2022-05-16 10:30:04 +0100

    multi-statement running and rerunning queries and creating views

--- src/test/scala/com/databeans/MultiStatementUtilsSpec.scala
+++ src/test/scala/com/databeans/MultiStatementUtilsSpec.scala
@@ -1,5 +1,60 @@
 package com.databeans
 
-class MultiStatementUtilsSpec {
+import org.apache.spark.sql.QueryTest
+import org.apache.spark.sql.delta.test.DeltaExtendedSparkSession
+import org.apache.spark.sql.test.SharedSparkSession
+import com.databeans.MultiStatementUtils._
+import org.apache.spark.sql.functions.col
 
+
+case class Data(value: Long, keys: Long, option: Long)
+
+class MultiStatementUtilsSpec extends QueryTest
+  with SharedSparkSession
+  with DeltaExtendedSparkSession {
+  test("beginTransaction should run multiple non-failing SQL queries") {
+    val s = spark
+    import s.implicits._
+
+    val data = Seq(1, 5).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
+    data.write.mode("overwrite").format("delta").saveAsTable("my_fake_tab")
+    val updatesData = Seq(9, 5).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
+    updatesData.write.mode("overwrite").format("delta").saveAsTable("updates")
+
+    val deleteQuery = "DELETE FROM updates WHERE value = 5"
+    val updateQuery = "UPDATE my_fake_tab SET value = 6 WHERE value = 1"
+
+    beginTransaction(spark, Array(deleteQuery, updateQuery), Array("updates", "my_fake_tab"))
+    Thread.sleep(5000)
+
+    val updateResult = spark.sql("select * from updates_view")
+    val expectedUpdateResult = Seq(Data(9, 18, 27), Data(98, 196, 294), Data(102, 204, 306), Data(100, 200, 300), Data(1, 2, 3)).toDF()
+
+    val myFakeTabResult = spark.sql("select * from my_fake_tab_view")
+    val expectedMyFakeTabResult = Seq(Data(6, 2, 3), Data(5, 10, 15)).toDF()
+
+    assert(updateResult.except(expectedUpdateResult).isEmpty)
+    assert(myFakeTabResult.except(expectedMyFakeTabResult).isEmpty)
+  }
+
+  test("createViewsInCaseOfFailure should run multiple SQL queries with consistent views if computation fails") {
+    val s = spark
+    import s.implicits._
+
+    val data = Seq(1, 5).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
+    data.write.mode("overwrite").format("delta").saveAsTable("my_fake_tab")
+    val updatesData = Seq(9, 5).toDF().withColumn("keys", col("value") * 2).withColumn("option", col("value") * 3)
+    updatesData.write.mode("overwrite").format("delta").saveAsTable("updates")
+
+    val deleteQuery = "DELETE FROM updates WHERE value = 5"
+    val selectQuery = "select * from my_fake_tab" //to mimic a non-committed change i.e tableVersion won't change
+
+    beginTransaction(spark, Array(deleteQuery, selectQuery), Array("updates", "my_fake_tab"))
+    Thread.sleep(5000)
+
+    createViewsInCaseOfFailure(spark, Array("updates", "my_fake_tab"))
+    val result = spark.sql("select * from updates_view")
+    val expectedResult = Seq(Data(9, 18, 27), Data(5, 10, 15)).toDF()
+    assert(result.except(expectedResult).isEmpty)
+  }
 }
