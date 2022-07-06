97a3b2faeaed79516b52f629a5d7604b8fccef29
gargouriahmed <ahmed.gargouri@dsoservices.com>
2022-07-06 15:01:49 +0100

    fixing views malfunction

--- src/main/scala/com/databeans/MultiStatement.scala
+++ src/main/scala/com/databeans/MultiStatement.scala
@@ -5,8 +5,8 @@ import com.databeans.MultiStatementUtils._
 object MultiStatement {
 
   def multiStatementTransaction(spark: SparkSession, transactions: Array[String], tableNames: Array[String]): Unit = {
-    //val tableStates = createUniqueTableName(tableNames)
-    beginTransaction(spark, transactions, tableNames, "tableStates")
+    val tableStates = createUniqueTableName(tableNames)
+    beginTransaction(spark, transactions, tableNames, tableStates)
   }
 }
 
